883a1a2dca92fd0eca0085c887e73642e84c9c4a
rkdwlss2 <rkdwlss2@gmail.com>
2026-09-17 11:28:36 +0900

    refactor: use request DTO records for auth flows

---
+++ src/main/java/com/homes/zipsai/user/dto/UserPatchRequest.java
@@ -0,0 +1,16 @@
+package com.homes.zipsai.user.dto;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.homes.zipsai.auth.dto.AgreementRequest;
+import com.homes.zipsai.user.domain.UserRole;
+
+@JsonIgnoreProperties(ignoreUnknown = false)
+public record UserPatchRequest(
+    UserRole userRole,
+    String userName,
+    String phone,
+    List<AgreementRequest> agreements
+) {
+}
