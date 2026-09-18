08262b1c58ded705865e832e5b5e17c6c1e2a0ab
LunaXion00 <lunaof12@gmail.com>
2026-09-18 20:08:40 +0900

    feat: 관리자 민원 조회 저장소 구현 (#26)

--- src/main/java/com/homes/zipsai/building/repository/BuildingRepository.java
+++ src/main/java/com/homes/zipsai/building/repository/BuildingRepository.java
@@ -1,5 +1,7 @@
 package com.homes.zipsai.building.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.homes.zipsai.building.domain.Building;
@@ -7,4 +9,6 @@ import com.homes.zipsai.building.domain.Building;
 public interface BuildingRepository extends JpaRepository<Building, Long> {
     // 매니저가 이미 건물을 등록했는지 전체 엔티티 조회 없이 확인합니다.
     boolean existsByManager_Id(Long managerId);
+
+    Optional<Building> findByManager_IdAndDeletedAtIsNull(Long managerId);
 }
