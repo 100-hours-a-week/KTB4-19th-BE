f98949da351497deed2dc698ca7734b1d4ab57b2
rkdwlss2 <rkdwlss2@gmail.com>
2026-09-21 14:37:30 +0900

    Revert "Merge pull request #41 from 100-hours-a-week/fix/remove-duplicate-spring-run"
    This reverts commit eb09fe6ac54befb59e2bde1688664a2d6e7fa931, reversing
    changes made to 1d005cc69fcba968e319899769b08e975868b701.

--- src/main/java/com/homes/zipsai/ZipsaiBackendApplication.java
+++ src/main/java/com/homes/zipsai/ZipsaiBackendApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ZipsaiBackendApplication {
+	static void main(String[] args) {
+		SpringApplication.run(ZipsaiBackendApplication.class, args);
+	}
+
     public static void main(String[] args) {
         SpringApplication.run(ZipsaiBackendApplication.class, args);
     }
