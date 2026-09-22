69eb8349acd95c9aa7ed259c514bb6a8c4884a4e
LunaXion00 <lunaof12@gmail.com>
2026-09-22 17:40:16 +0900

    feat: 약관 조회 응답 DTO 추가 (#55)

---
+++ src/main/java/com/homes/zipsai/user/dto/TermsListResponse.java
@@ -0,0 +1,11 @@
+package com.homes.zipsai.user.dto;
+
+import java.util.List;
+
+import com.homes.zipsai.user.domain.TermsType;
+
+public record TermsListResponse(List<TermsItem> terms) {
+
+    public record TermsItem(TermsType termsType, String title) {
+    }
+}
